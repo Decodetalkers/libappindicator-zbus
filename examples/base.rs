1ff70ca0bebd09d76ebd1462c5424be6f445db3b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-09-23 19:07:35 +0900

    chore: init

---
+++ examples/base.rs
@@ -0,0 +1,47 @@
+use status_notifier::status_notifier_item::application;
+use zbus::fdo::Result;
+struct Base;
+
+impl Base {
+    fn boot() -> Self {
+        Base
+    }
+    fn id() -> String {
+        "Hello".to_owned()
+    }
+    fn activate(&self, x: i32, y: i32) -> Result<()> {
+        println!("active");
+        Ok(())
+    }
+    fn context_menu(&self, x: i32, y: i32) -> Result<()> {
+        Ok(())
+    }
+    fn scroll(&self, delta: i32, orientation: &str) -> Result<()> {
+        Ok(())
+    }
+    fn secondary_activate(&self, x: i32, y: i32) -> Result<()> {
+        Ok(())
+    }
+    fn icon_name(&self) -> Result<String> {
+        Ok("nheko".to_owned())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let _connection = application(
+        Base::boot,
+        Base::id,
+        Base::activate,
+        Base::context_menu,
+        Base::scroll,
+        Base::secondary_activate,
+    )
+    .with_icon_name(Base::icon_name)
+    .run()
+    .await
+    .unwrap();
+    println!("{:?}", _connection.unique_name());
+
+    std::future::pending::<()>().await;
+}
